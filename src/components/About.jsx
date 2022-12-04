0b03a86751a3c3bfde4f12a06dcfe8b47fad59c1
Mathias Skovgaard <sk0vgaard@live.dk>
2022-12-04 14:17:47 +0100

    - Add about me

---
+++ src/components/About.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+
+const About = () => {
+    return (
+        <div className='w-full my-32 '>
+            <div className='max-w-[1240px] mx-auto'>
+                <div className='text-center'>
+                    <h2 className='text-5xl font-bold'>A developer with flare</h2>
+                    <p className='text-3xl py-6 text-gray-500'>Love to fish and talk</p>
+                </div>
+
+                <div className='grid md:grid-cols-3 gap-1 px-2 text-center'>
+                    <div className='border py-8 rounded-xl shadow-xl'>
+                        <p className='text-6xl font-bold text-indigo-600'>Test</p>
+                        <p className='text-gray-400 mt-2'>Good code is tested code</p>
+                    </div>
+                    <div className='border py-8 rounded-xl shadow-xl'>
+                        <p className='text-6xl font-bold text-indigo-600'>100%</p>
+                        <p className='text-gray-400 mt-2'>Commitment</p>
+                    </div>
+                    <div className='border py-8 rounded-xl shadow-xl'>
+                        <p className='text-6xl font-bold text-indigo-600'>5 Years</p>
+                        <p className='text-gray-400 mt-2'>Frontend Experience</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default About;
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -13,7 +13,7 @@ const Hero = () => {
                     <button className='py-3 px-6 sm:w-[60%] my-4'>Get Started</button>
                 </div>
                 <div>
-                    <img className='w-full md:max-w-[474px]' src={bgImg} alt="/" />
+                    <img className='w-full md:max-w-[474px] sm:max-w-[650px] ' src={bgImg} alt="/" />
                 </div>
                 <div className='absolute flex flex-col py-8 md:min-w-[760px] bottom-[5%]
             mx-1 md:left-1/2 transform md:-translate-x-1/2 bg-zinc-200
