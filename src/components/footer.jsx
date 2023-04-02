c445c495c5e20c6b4a0bcfa5d151443ee074d04c
Skovgaard <sk0vgaard@live.dk>
2023-04-02 20:24:48 +0200

    - Add links for icons - Add tools that has been used to create the website

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,71 +1,54 @@
 import React from 'react';
-import {FaFacebook, FaGithub, FaInstagram} from "react-icons/fa";
+import {FaGithub, FaInstagram, FaLinkedinIn, FaReact} from 'react-icons/fa';
+import tailwind from './../assets/tailwind.png'
+import react from './../assets/react.png'
+import firebase from './../assets/firebase.png'
+
 
 const Footer = () => {
+    const linkedInUrl = "https://www.linkedin.com/in/mathias-skovgaard/";
+    const githubUrl = "https://github.com/Sk0vgaard";
+    const instagramUrl = "https://www.instagram.com/sk0vgaard/";
+
     return (
-        <div className='w-full mt-24 bg-slate-900 text-gray-300 py-y px-2'>
-            <div className='max-w-[1240px] mx-auto grid grid-cols-2 md:grid-cols-6 border-b-2 border-gray-600 py-8'>
-                <div>
-                    <h6 className='font-bold uppercase pt-2'>Solutions</h6>
-                    <ul>
-                        <li className='py-1'>Marketing</li>
-                        <li className='py-1'>Analytics</li>
-                        <li className='py-1'>Commerce</li>
-                        <li className='py-1'>Data</li>
-                        <li className='py-1'>Cloud</li>
-                    </ul>
+        <div className="w-full mt-24 bg-slate-900 py-8 px-2">
+            <div className="max-w-[1240px] mx-auto grid md:grid-cols-2 border-b-2 border-gray-600 py-8">
+                <div className="flex items-center justify-center h-full">
+                    <h1 className="text-xl font-semibold mb-4 text-center text-gray-400 m-0">
+                        This website has been created with
+                    </h1>
                 </div>
-                <div>
-                    <h6 className='font-bold uppercase pt-2'>Support</h6>
-                    <ul>
-                        <li className='py-1'>Marketing</li>
-                        <li className='py-1'>Analytics</li>
-                        <li className='py-1'>Commerce</li>
-                        <li className='py-1'>Data</li>
-                        <li className='py-1'>Cloud</li>
-                    </ul>
-                </div>
-                <div>
-                    <h6 className='font-bold uppercase pt-2'>Contact</h6>
-                    <ul>
-                        <li className='py-1'>Marketing</li>
-                        <li className='py-1'>Analytics</li>
-                        <li className='py-1'>Commerce</li>
-                        <li className='py-1'>Data</li>
-                        <li className='py-1'>Cloud</li>
-                    </ul>
-                </div>
-                <div>
-                    <h6 className='font-bold uppercase pt-2'>Documentation</h6>
-                    <ul>
-                        <li className='py-1'>Marketing</li>
-                        <li className='py-1'>Analytics</li>
-                        <li className='py-1'>Commerce</li>
-                        <li className='py-1'>Data</li>
-                        <li className='py-1'>Cloud</li>
-                    </ul>
-                </div>
-                <div className='col-span-2 pt-8 md:pt-2'>
-                    <p className='font-bold uppercase'>Subscribe to newsletter</p>
-                    <p className='pt-4'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid atque id
-                        mollitia nesciunt, qui quia sint! Hic incidunt libero magnam.</p>
-                    <form className='flex flex-col sm:flex-row'>
-                        <input className='w-full p-2 mr-4 rounded-md mb-4' type="email" placeholder='Enter E-mail'/>
-                        <button className='p-2 mb-4'>Subscribe</button>
-                    </form>
+
+                <div className="flex justify-center space-x-4">
+                    <img src={react} alt="" className={'h-10'}/>
+                    <img src={tailwind} alt="" className={'h-10'}/>
+                    <img src={firebase} alt="" className={'h-10'}/>
                 </div>
             </div>
 
-            <div className='flex flex-col max-w-[1240px] px-10 py-4 mx-auto justify-between sm:flex-row text-center text-gray-400'>
-                <p className='py-4'>© All rights reserved</p>
-                <div className='flex justify-between sm:w-[300px] pt-2 text-3xl'>
-                    <FaFacebook/>
-                    <FaInstagram/>
-                    <FaGithub/>
+            <div
+                className="flex flex-col max-w-[1240px] px-10 py-4 mx-auto justify-between sm:flex-row text-center text-gray-400">
+                <p className="py-4">© All rights reserved</p>
+                <div className='flex justify-between flex-wrap px-20'>
+                    <div className='flex px-4 py-2 text-slate-500'>
+                        <a href={linkedInUrl} target="_blank" rel="noopener noreferrer">
+                            <FaLinkedinIn size={'40px'} className='text-slate-400'/>
+                        </a>
+                    </div>
+                    <div className='flex px-4 py-2 text-slate-500'>
+                        <a href={githubUrl} target="_blank" rel="noopener noreferrer">
+                            <FaGithub size={'40px'} className='text-slate-400'/>
+                        </a>
+                    </div>
+                    <div className='flex px-4 py-2 text-slate-500'>
+                        <a href={instagramUrl} target="_blank" rel="noopener noreferrer">
+                            <FaInstagram size={'40px'} className='text-slate-400'/>
+                        </a>
+                    </div>
                 </div>
             </div>
         </div>
-    )
-}
+    );
+};
 
 export default Footer;
