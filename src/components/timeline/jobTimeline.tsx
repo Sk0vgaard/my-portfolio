bfaa125e18967358bc1d0da0026fe82f5af61a07
Skovgaard <sk0vgaard@live.dk>
2023-04-26 18:03:12 +0200

    Add icons for expand/collaps card

--- src/components/timeline/jobTimeline.tsx
+++ src/components/timeline/jobTimeline.tsx
@@ -1,12 +1,12 @@
 import React, {useState} from 'react';
-import JobTitleTimeline from "./jobTitleTimeline";
-import {FaBuilding} from "react-icons/fa";
+import JobTitleTimeline from './jobTitleTimeline';
+import {FaBuilding} from 'react-icons/fa';
+import {FiChevronDown, FiChevronUp} from 'react-icons/fi';
 import {CompanyTimeline} from '../../interfaces';
 
 type ExpandedState = {
     [key: string]: boolean;
 };
-
 
 const JobTimeline = ({companyTimelines}: { companyTimelines: CompanyTimeline[] }) => {
     const [expanded, setExpanded] = useState<ExpandedState>({});
@@ -19,12 +19,12 @@ const JobTimeline = ({companyTimelines}: { companyTimelines: CompanyTimeline[] }
     };
 
     return (
-        <ol className="border-l-2 border-primary dark:border-primary-500">
+        <ol className="border-l-2 border-primary border-primary-500">
             {companyTimelines.map((companyTimeline: CompanyTimeline) => {
                 const itemKey = companyTimeline.companyName;
 
                 return (
-                    <li>
+                    <li key={itemKey}>
                         <div className="flex-start items-center">
                             <div
                                 className="-ml-[36px] flex h-[40px] w-[40px] items-center justify-center rounded-full bg-info-100 text-info-700">
@@ -37,11 +37,9 @@ const JobTimeline = ({companyTimelines}: { companyTimelines: CompanyTimeline[] }
                                     <FaBuilding></FaBuilding>
                                 </svg>
                             </div>
-                            <div
-                                key={itemKey}
-                                onClick={() => toggleExpand(itemKey)}
-                                className="block rounded-lg bg-neutral-50 p-4 md:p-6 shadow-md shadow-black/5 dark:bg-neutral-700 dark:shadow-black/10 cursor-pointer hover:bg-slate-900">
-                                <div className="flex items-center md:justify-between">
+                            <div onClick={() => toggleExpand(itemKey)}
+                                 className="block rounded-lg bg-neutral-50 p-4 md:p-6 shadow-md bg-neutral-700 cursor-pointer hover:bg-neutral-800">
+                                <div className="flex items-center justify-center md:justify-between">
                                     <span className="text-sm text-info" rel="noreferrer">
                                         <p className="hidden md:block text-3xl">
                                             {companyTimeline.companyName}
@@ -59,14 +57,22 @@ const JobTimeline = ({companyTimelines}: { companyTimelines: CompanyTimeline[] }
                                 {expanded[itemKey] && (
                                     <JobTitleTimeline jobDetails={companyTimeline.details}/>
                                 )}
-                                <div className={'py-2 pl-4'}>
+                                <div className={'py-2 md:pl-4 flex items-center justify-center md:justify-between'}>
                                     <button
                                         type="button"
-                                        className="inline-block rounded border-2 border-info px-4 pt-[4px] pb-[3px] text-s font-medium uppercase leading-normal text-info transition duration-150 ease-in-out hover:border-info-600 hover:bg-neutral-500 hover:bg-opacity-10 hover:text-info-600 focus:border-info-600 focus:text-info-600 focus:outline-none focus:ring-0 active:border-info-700 active:text-info-700 dark:hover:bg-neutral-100 dark:hover:bg-opacity-10">
+                                        className="inline-block rounded border-2 border-info px-4 pt-[4px] pb-[3px] text-s font-medium uppercase leading-normal text-info transition duration-150 ease-in-out hover:border-info-600 hover:bg-neutral-500 hover:bg-opacity-10 hover:text-info-600 focus:border-info-600 focus:text-info-600 focus:outline-none focus:ring-0 active:border-info-700 active:text-info-700 hover:bg-neutral-100 hover:bg-opacity-10"
+                                    >
                                         <a href={companyTimeline.companyLink} target="_blank" rel="noreferrer">
                                             Read about {companyTimeline.companyName}
                                         </a>
                                     </button>
+                                </div>
+                                <div className="flex items-center justify-center">
+                                    {expanded[itemKey] ? (
+                                        <FiChevronUp className="text-info"/>
+                                    ) : (
+                                        <FiChevronDown className="text-info"/>
+                                    )}
                                 </div>
                             </div>
                         </div>
--- src/components/timeline/jobTitleTimeline.tsx
+++ src/components/timeline/jobTitleTimeline.tsx
@@ -3,18 +3,18 @@ import {JobDetails} from '../../interfaces';
 
 const JobTimeline = ({jobDetails}: { jobDetails: JobDetails[] }) => {
     return (
-        <ol className="border-l-2 border-primary dark:text-info">
+        <ol className="border-l-2 border-primary text-info">
             {jobDetails.map((jobDetail, index) => (
                 <li key={index}>
                     <div className="flex-start flex">
                         <div
-                            className="-ml-[27px] flex h-[20px] w-[20px] items-center justify-center rounded-full bg-primary dark:bg-info"></div>
+                            className="-ml-[27px] flex h-[20px] w-[20px] items-center justify-center rounded-full bg-primary bg-info"></div>
                         <div className={'pl-2 w-full'}>
                             <div className="mb-4 flex justify-between">
                                 <span>{jobDetail.title}</span>
                                 <span className="text-sm text-info transition duration-150 text-info-500"> {jobDetail.fromDate} - {jobDetail.toDate} </span>
                             </div>
-                            <p className="mb-6 text-neutral-700 dark:text-neutral-200 whitespace-pre-wrap">
+                            <p className="mb-6 text-neutral-200 whitespace-pre-wrap">
                                 {jobDetail.description}
                             </p>
                         </div>
